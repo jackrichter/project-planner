d7b6be89b60680af0c7ce7df8a85ceeb673f48c1
jackrichter <jackrichter1@gmail.com>
2024-05-22 00:22:30 +0200

    A more complex OOP in JS example.

---
+++ assets/scripts/app.js
@@ -0,0 +1,157 @@
+class DOMHelper {
+	static moveElement(elementId, newDestinationSelector) {
+		const element = document.getElementById(elementId);
+		const destinationElement = document.querySelector(newDestinationSelector);
+		destinationElement.append(element);
+	}
+
+	static clearEventListeners(element) {
+		const clonedElement = element.cloneNode(true);
+		// The cloned element doesn't have it's previous event listeners
+		element.replaceWith(clonedElement);
+		return clonedElement;
+	}
+}
+
+class Component {
+	hostElement;
+	insertBefore;
+
+	constructor(hostElementId, insertBefore = false) {
+		if (hostElementId) {
+			this.hostElement = document.getElementById(hostElementId);
+		} else {
+			this.hostElement = document.body;
+		}
+		this.insertBefore = insertBefore;
+	}
+
+	detach() {
+		if (this.element) {
+			this.element.remove();
+
+			// For older Browsers
+			// this.element.parentElement.removeChild(this.element);
+		}
+	}
+
+	attach() {
+		// document.body.append(this.element);
+		this.hostElement.insertAdjacentElement(
+			this.insertBefore ? "afterbegin" : "beforeend",
+			this.element
+		);
+	}
+}
+
+class Tooltip extends Component {
+	element;
+
+	constructor(closeNotifierFn) {
+		super("finished-projects", true);
+		this.closeNotifier = closeNotifierFn;
+		this.create();
+	}
+
+	// The 'this' will always refer to the class when using an arrow function (no need to bind 'this' to the class in the event listener)!
+	closeTooltip = () => {
+		this.detach();
+		this.closeNotifier();
+	};
+
+	create() {
+		const tooltipElement = document.createElement("div");
+		tooltipElement.className = "card";
+		tooltipElement.textContent = "DUMMY!";
+		tooltipElement.addEventListener("click", this.closeTooltip);
+		this.element = tooltipElement;
+	}
+}
+
+class ProjectItem {
+	hasActiveTooltip = false;
+
+	constructor(id, updateProjectListsFunction, type) {
+		this.id = id;
+		this.updateProjectListsHandler = updateProjectListsFunction;
+		this.connectInfoButton();
+		this.connectSwitchButton(type);
+	}
+
+	showMoreInfoHandler() {
+		if (this.hasActiveTooltip) {
+			return;
+		}
+		const tooltip = new Tooltip(() => (this.hasActiveTooltip = false));
+		tooltip.attach();
+		this.hasActiveTooltip = true;
+	}
+
+	connectInfoButton() {
+		const projectItemElement = document.getElementById(this.id);
+		const moreInfoBtn = projectItemElement.querySelector("button:first-of-type");
+		moreInfoBtn.addEventListener("click", this.showMoreInfoHandler);
+	}
+
+	// Switch refers to the finish button
+	connectSwitchButton(type) {
+		const projectItemElement = document.getElementById(this.id);
+		let switchBtn = projectItemElement.querySelector("button:last-of-type");
+		// Do not accumulate event listeners. Always, use a fresh one
+		switchBtn = DOMHelper.clearEventListeners(switchBtn);
+		switchBtn.textContent = type === "active" ? "Finish" : "Activate";
+		switchBtn.addEventListener("click", this.updateProjectListsHandler.bind(null, this.id));
+	}
+
+	update(updateProjectListsFn, type) {
+		this.updateProjectListsHandler = updateProjectListsFn;
+		this.connectSwitchButton(type);
+	}
+}
+
+class ProjectList {
+	projects = [];
+
+	constructor(type) {
+		this.type = type;
+		const prjItems = document.querySelectorAll(`#${type}-projects li`);
+		for (const item of prjItems) {
+			this.projects.push(new ProjectItem(item.id, this.switchProject.bind(this), this.type));
+		}
+	}
+
+	setSwitchHandlerFunction(switchHandlerFunction) {
+		this.switchHandler = switchHandlerFunction;
+	}
+
+	// Add to the other project's list
+	addProject(project) {
+		this.projects.push(project);
+		DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
+		project.update(this.switchProject.bind(this), this.type);
+	}
+
+	// Remove from one project's list
+	switchProject(projectId) {
+		// const prjIndex = this.projects.findIndex((p) => p.id === projectId);
+		// this.projects.splice(prjIndex, 1);
+		this.switchHandler(this.projects.find((p) => p.id === projectId));
+		this.projects = this.projects.filter((prj) => prj.id !== projectId);
+	}
+}
+
+class App {
+	static init() {
+		const activeProjectList = new ProjectList("active");
+		const finishedProjectList = new ProjectList("finished");
+
+		activeProjectList.setSwitchHandlerFunction(
+			finishedProjectList.addProject.bind(finishedProjectList)
+		);
+		finishedProjectList.setSwitchHandlerFunction(
+			activeProjectList.addProject.bind(activeProjectList)
+		);
+	}
+}
+
+App.init();
