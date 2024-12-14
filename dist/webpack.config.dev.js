9cf597fca2a39fe1ce7b50b6600e02b44326995d
jackrichter <jackrichter1@gmail.com>
2024-12-14 14:10:43 +0100

    Webpack-dev-server (not working for me).

--- dist/webpack.config.dev.js
+++ dist/webpack.config.dev.js
@@ -8,7 +8,14 @@ module.exports = {
   entry: "./src/app.js",
   output: {
     filename: "app.js",
-    path: path.resolve(__dirname, "assets", "scripts"),
-    publicPath: "assets/scripts/"
+    path: path.resolve(__dirname, "assets", "scripts") // publicPath: "assets/scripts/"
+
+  },
+  devServer: {
+    "static": {
+      directory: path.join(__dirname, "assets", "scripts")
+    },
+    compress: true,
+    port: 8080
   }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,14 @@ module.exports = {
 	entry: "./src/app.js",
 	output: {
 		filename: "app.js",
-		path: path.resolve(__dirname, "assets", "scripts"),
-		publicPath: "assets/scripts/"
+		path: path.resolve(__dirname, "assets", "scripts")
+		// publicPath: "assets/scripts/"
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "assets", "scripts")
+		},
+		compress: true,
+		port: 8080
 	}
 };
